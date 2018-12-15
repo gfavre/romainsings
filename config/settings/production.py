4d42fb827f30ac2fe2bb9edc61e9147a654bf085
Gregory Favre <greg@beyondthewall.ch>
2018-12-15 16:54:12 +0100

    imp - prod settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['romainpi.local', '0.0.0.0:5000', 'romainpi.local:5000'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['romainpi.local', '0.0.0.0'])
 
 # DATABASES
 # ------------------------------------------------------------------------------
