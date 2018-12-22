0a50fc9d8d775c043cd05f62c4f25034822f3cd4
Gregory Favre <greg@beyondthewall.ch>
2018-12-22 14:34:30 +0100

    imp - add player

--- romain_sings/player/management/commands/wait_commands.py
+++ romain_sings/player/management/commands/wait_commands.py
@@ -6,21 +6,22 @@ from django.core.management.base import BaseCommand, CommandError
 from ...qrplay import handle_qrcode
 
 
-def start_scan(process):
+def start_scan(process, outstream):
     while True:
         data = process.readline()
         qrcode = str(data)[8:]
         if qrcode:
+            outstream.write('Read {}\n'.format(qrcode))
             qrcode = qrcode.rstrip()
             handle_qrcode(qrcode)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        p = os.popen('/usr/bin/zbarcam --prescale=300x200', 'r')
+        p = os.popen('/usr/bin/zbarcam --prescale=300x200 --nodisplay', 'r')
         try:
             self.stdout.write('opening software')
-            start_scan(p)
+            start_scan(self.stdout)
         except KeyboardInterrupt:
             self.stdout.write('Stopping scanner...')
         finally:
